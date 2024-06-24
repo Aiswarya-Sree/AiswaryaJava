2acaa765e1184e9a51e635d4e0d84a40e9e2da7e
aiswarya.sree1 <aiswarya.sree1@SPCLTLAP-0422.speridian.com>
2024-06-24 20:53:21 +0530

    test 1

--- Mavenproject/src/main/java/inheritance/ChildClass.java
+++ Mavenproject/src/main/java/inheritance/ChildClass.java
@@ -6,7 +6,7 @@ public class ChildClass extends ParentClass {
 		System.out.println("child class");
 	}
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		// TODO Auto-generated method stub
 		ChildClass c1=new ChildClass();
 		c1.sum();
