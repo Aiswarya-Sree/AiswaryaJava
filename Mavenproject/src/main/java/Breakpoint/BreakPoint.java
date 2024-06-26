ecc0802c1e5f0d82de19e900e891cd254d63b01d
aiswarya.sree1 <aiswarya.sree1@SPCLTLAP-0422.speridian.com>
2024-06-26 19:55:50 +0530

    breakpoint pgm added

---
+++ Mavenproject/src/main/java/Breakpoint/BreakPoint.java
@@ -0,0 +1,13 @@
+package Breakpoint;
+
+public class BreakPoint {
+static int a;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(a=0;a<=10;a++)
+{
+	System.out.println(a);
+}
+	}
+
+}
